83b29e261a0e553898b330b2d4b29883f1abfb96
cogphn <jason.deyalsingh@gmail.com>
2025-08-19 15:47:27 -0400

    Update main.rs

--- ro1/src/main.rs
+++ ro1/src/main.rs
@@ -44,7 +44,7 @@ unsafe extern "system" fn event_callback(
                  )
              };
             let xml = String::from_utf8_lossy(&buffer);
-            println!("Event XML: {}", xml);
+            println!("{}", xml);
         }
         _ => {
             // Handle other actions like EvtSubscribeActionError
@@ -54,15 +54,16 @@ unsafe extern "system" fn event_callback(
     0 // Return 0 to indicate success
 }
 
-fn get_evt_handle(elog_channel_path:&str) -> Result<Win32::System::EventLog::EVT_HANDLE> {
+fn get_evt_sub_handle(elog_channel_path:&str, query: &str) -> Result<Win32::System::EventLog::EVT_HANDLE> {
     let channel_path: HSTRING = HSTRING::from(elog_channel_path);
+    let q = HSTRING::from(query);
 
     let elog_sub_handle = unsafe {
         EvtSubscribe(
             None,
             None,
             PCWSTR::from_raw(channel_path.as_ptr()),
-            PCWSTR::from_raw(HSTRING::from("*").as_ptr()),
+            PCWSTR::from_raw(q.as_ptr()),
             None,
             None,
             Some(event_callback),
@@ -71,6 +72,12 @@ fn get_evt_handle(elog_channel_path:&str) -> Result<Win32::System::EventLog::EVT
     };
 
     return elog_sub_handle;
+}
+
+#[derive(Debug)]
+struct ElogChannel {
+    channel_name: String,
+    query: String
 }
 
 fn main() -> Result<()> {
@@ -83,60 +90,30 @@ fn main() -> Result<()> {
         println!("Ctrl+C received, shutting down...");
     }).expect(" [!] some kind of error presumably.... shutting down");
     
-    //let application_channel_path = HSTRING::from("Application");
-    let system_channel_path = HSTRING::from("System");    
     
-    let query = HSTRING::from("*");
-    
+    println!("[*] Subscribing to Windows Event Logs...");
 
-    println!("[*] Subscribing to Windows Event Log...");
-
-    // application eventlog 
-    /*
-    let app_subscription = unsafe {
-        EvtSubscribe(
-            None,
-            None,
-            PCWSTR::from_raw(application_channel_path.as_ptr()),
-            PCWSTR::from_raw(query.as_ptr()),
-            None,
-            None,
-            Some(event_callback),
-            //EvtSubscribeStartAtOldestRecord.0,
-            EvtSubscribeToFutureEvents.0,
-        )
-    };
-     */
-    let app_subscription = get_evt_handle("Application");
-
-
+    let app_subscription = get_evt_sub_handle("Application", "*");
     if app_subscription.clone()?.is_invalid() {
         let error = windows::core::Error::from_win32();
         eprintln!(" [!] Failed to subscribe to event log: {:?}", error);
         return Err(error);
     }
-
     // system event log 
-    let sys_subscription = unsafe {
-        EvtSubscribe(
-            None,
-            None,
-            PCWSTR::from_raw(system_channel_path.as_ptr()),
-            PCWSTR::from_raw(query.as_ptr()),
-            None,
-            None,
-            Some(event_callback),            
-            EvtSubscribeToFutureEvents.0,
-        )
-    };
-
+    let sys_subscription = get_evt_sub_handle("System", "*");
     if sys_subscription.clone()?.is_invalid() {
         let error = windows::core::Error::from_win32();
         eprintln!(" [!] Failed to subscribe to event log: {:?}", error);
         return Err(error);
     }
-    
 
+    let sec_subscription = get_evt_sub_handle("Security", "*");
+    if sec_subscription.clone()?.is_invalid() {
+        let error = windows::core::Error::from_win32();
+        eprintln!(" [!] Failed to subscribe to event log: {:?}", error);
+        return Err(error);
+    }
+    
     
     
     println!("[*] listening for events. Press Ctrl+C to stop.");
