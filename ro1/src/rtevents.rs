6e14ddda520a2e3633c9311430f0cfcc0a0a8231
cogphn <jason.deyalsingh@gmail.com>
2025-08-30 10:19:31 -0400

    Update rtevents.rs

--- ro1/src/rtevents.rs
+++ ro1/src/rtevents.rs
@@ -8,8 +8,11 @@ use std::collections::HashMap;
 use futures::StreamExt;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
-use chrono::{DateTime, NaiveDateTime, ParseError, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
+use std::sync::OnceLock;
 
+use super::parser;
+use super::cache;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename = "Win32_Process")] 
@@ -27,25 +30,49 @@ struct Process {
 
 #[derive(Deserialize,Debug)]
 #[derive(Serialize)]
-struct ProcessInfo {
-    process_id: u32,
-    name: String,
-    executable_path: String,
-    command_line: String,
-    parent_process_id: u32,
-    creation_date: NaiveDateTime
+pub struct ProcessInfo {
+    pub process_id: u32,
+    pub hostname: String,
+    pub name: String,
+    pub executable_path: String,
+    pub command_line: String,
+    pub parent_process_id: u32,
+    pub creation_date: NaiveDateTime
 }
 
-/*
-#[derive(Deserialize, Debug)]
-#[serde(rename = "__InstanceCreationEvent")]
-#[serde(rename_all = "PascalCase")]
-struct NewProcessEvent {
-    target_instance: Process
-}
- */
-
+pub static HOSTNAME: OnceLock<String> = OnceLock::new();
 pub static mut RUNNING: AtomicBool = AtomicBool::new(true);
+
+pub fn get_hostname() -> String {
+    if let Some(hostname) = HOSTNAME.get() {
+        return hostname.to_string();
+    }
+
+    let com_lib = match COMLibrary::new(){
+        Ok(v) => v,
+        Err(_) => return "Unknown".to_string()
+    };
+    let wmi_con = match WMIConnection::new(com_lib){
+        Ok(v) => v,
+        Err(_) => return "Unknown".to_string()
+    };    
+    let wmi_computersystem: Vec<HashMap<String, Variant>> = match wmi_con.raw_query("SELECT DNSHostName FROM Win32_ComputerSystem"){
+        Ok(v) => v,
+        Err(_) => return "Unknown".to_string()
+    };
+
+    if let Some(info) = wmi_computersystem.into_iter().next() {
+        if let Some(Variant::String(hostname)) = info.get("DNSHostName") {
+            HOSTNAME.set(hostname.to_string()).ok();
+            return hostname.to_string();
+        }
+    }
+    HOSTNAME.set("Unknown".to_string()).ok();
+    "Unknown".to_string()
+}
+
+
+
 
 
 pub fn get_process_list() -> wmi::WMIResult<()> {
@@ -57,7 +84,7 @@ pub fn get_process_list() -> wmi::WMIResult<()> {
 
     for process in processes {
         println!("Creation Date: {:?}, Process ID: {}, Parent Process ID: {} Name: {}, Path: {:?}, Command Line:{:?}",
-            process.creation_date.expect("1970-01-01T00:00:00").to_rfc3339(),
+            process.creation_date.expect("1970-01-01T00:00:00").to_rfc3339(),            
             process.process_id,
             process.parent_process_id,
             process.name,
@@ -84,7 +111,7 @@ pub fn get_process_listv2() -> Result<(), std::error::Error> {
 
 
 pub async fn process_observer(running: Arc<AtomicBool>) -> Result<(), Box<dyn std::error::Error>> {
-    println!("\nMonitoring for new process creation...\n");
+    println!(" [*] Monitoring for new process creation...\n");
     let com_lib = COMLibrary::new()?;
     let wmi_con = WMIConnection::new(com_lib)?;
     let new_proc_query = "SELECT * FROM Win32_ProcessStartTrace";
@@ -100,9 +127,22 @@ pub async fn process_observer(running: Arc<AtomicBool>) -> Result<(), Box<dyn st
         match process_start_stream.next().await {
             Some(event) => {
                 match event {
-                    Ok(process) => {
-                        //println!("New process started:");
-                        print_process_info(&process, "Win32_ProcessStartTrace");
+                    Ok(process) => {                        
+                        //print_process_info(&process, "Win32_ProcessStartTrace");
+                        let newproc = match parser::proc_hm_to_pi(&process, "Win32_ProcessStartTrace") {
+                            Ok(pi) => {
+                                println!("{}",serde_json::to_string(&pi).unwrap());    
+                                let parsed_procinfo = parser::pi_to_er(&pi);
+                                if let Ok(er) = parsed_procinfo {
+                                    let _ = cache::get_runtime().spawn(async move {
+                                        cache::insert_event(&er).await.ok();
+                                    });
+                                }
+                            },
+                            Err(e) => {
+                                eprintln!(" [!] Error parsing process details: {:?}", e);                                
+                            }
+                        };
                     }
                     Err(e) => eprintln!("Error receiving event: {:?}", e),
                 }
@@ -116,11 +156,15 @@ pub async fn process_observer(running: Arc<AtomicBool>) -> Result<(), Box<dyn st
 }
 
 
-fn print_process_info(process: &HashMap<String, Variant>, classname: &str) {
-    
 
+
+
+fn print_process_info(process: &HashMap<String, Variant>, classname: &str) {
+    /*
+    let hostname = get_hostname();
     let mut newproc: ProcessInfo = ProcessInfo { 
         name: "N/A".to_string(),
+        hostname: hostname,
         command_line: "N/A".to_string(),
         parent_process_id: 0,
         process_id: 0,
@@ -146,7 +190,7 @@ fn print_process_info(process: &HashMap<String, Variant>, classname: &str) {
         };
         newproc.process_id = match process.get("ProcessId") {
             Some(Variant::UI4(id)) => *id,
-            Some(Variant::String(s)) => 0,  //TODO: fix
+            Some(Variant::String(_s)) => 0,  //TODO: fix
             _ => 0,
         };
         newproc.creation_date = match process.get("CreationDate") {
@@ -167,14 +211,11 @@ fn print_process_info(process: &HashMap<String, Variant>, classname: &str) {
         };
         newproc.process_id = match process.get("ProcessID") {
             Some(Variant::UI4(id)) => *id,
-            Some(Variant::String(s)) => 0, 
+            Some(Variant::String(_s)) => 0, 
             _ => 0
         };
 
-        let pid_u32:u32 = match process.get("ProcessID") {
-            Some(Variant::UI4(id)) => *id,
-            _ => 0,
-        };
+
          
         let process_details = get_process_details(newproc.process_id);
         
@@ -200,39 +241,29 @@ fn print_process_info(process: &HashMap<String, Variant>, classname: &str) {
 
     }
 
+    let parsed_procinfo = parser::procdetails_to_er(&newproc);
+    let _ = cache::get_runtime().spawn(async move {
+        if let Ok(er) = parsed_procinfo {
+            cache::insert_event(&er).await.ok();
+        }
+    });
+
+    println!("{}",serde_json::to_string(&newproc).unwrap());    
+    */
+    let newproc = match parser::proc_hm_to_pi(process, classname) {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!(" [!] Error parsing process details: {:?}", e);
+            return;
+        }
+    };
     println!("{}",serde_json::to_string(&newproc).unwrap());    
 
 }
 
 
-fn convert_wmi_datetime(wmi_date: &str) -> String { //TODO: fix timezone issue 
-    if wmi_date.len() < 14 {
-        return "1970-01-01T00:00:00".to_string().into();
-    }
-    let dt_str = &wmi_date[0..14];    
-    if let Ok(naive_dt) = NaiveDateTime::parse_from_str(dt_str, "%Y%m%d%H%M%S") {
-        //Some(DateTime::from_utc(naive_dt, Utc))
-        let datetime: DateTime<Utc> = DateTime::from_utc(naive_dt, Utc);
-        return datetime.to_rfc3339().into();
-    } else {
-        return "1970-01-01T00:00:00".to_string().into();
-    }
-}
 
-
-fn convert_wmi_datetime_to_datetime(wmi_date: &str) -> Result<NaiveDateTime, ParseError> { 
-    if wmi_date.len() < 14 {
-        return NaiveDateTime::parse_from_str("1970-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S");
-    }
-    let dt_str = &wmi_date[0..14];    
-    if let Ok(naive_dt) = NaiveDateTime::parse_from_str(dt_str, "%Y%m%d%H%M%S") {
-        return Ok(naive_dt);
-    } else {
-        return NaiveDateTime::parse_from_str("1970-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S");
-    }
-}
-
-fn get_process_details(process_id: u32) -> Result<HashMap<String, Variant>, Box<dyn std::error::Error>> {
+pub fn get_process_details(process_id: u32) -> Result<HashMap<String, Variant>, Box<dyn std::error::Error>> {
     let com_con = COMLibrary::new()?;
     let wmi_con = WMIConnection::new(com_con)?;
 
