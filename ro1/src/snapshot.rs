aca58fb4a45edb88d255a6a26edfe7a14d87f7ef
cogphn <jason.deyalsingh@gmail.com>
2025-09-24 04:42:15 -0400

    Update snapshot.rs

--- ro1/src/snapshot.rs
+++ ro1/src/snapshot.rs
@@ -1,9 +1,25 @@
 //use chrono::{DateTime, Utc, NaiveDateTime, ParseError }; //, ParseError};
 use wmi::{COMLibrary, WMIConnection};
 
+use netstat_esr::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo};
+use serde::{Serialize, Deserialize};
+
 use super::rtevents;
 use super::parser;
 
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Netconn {
+    pub hostname: String,
+    pub conntype: String,
+    pub local_address: String,
+    pub local_port: u16,
+    pub remote_address: String,
+    pub remote_port: u16,
+    pub state: String,
+    pub associated_pids: Vec<u32>,
+    pub associated_processes: Vec<rtevents::Process>
+}
 
 
 
@@ -41,3 +57,55 @@ pub fn get_process_list() -> Result<Vec<rtevents::ProcessInfo>, Box<dyn std::err
     return Ok(process_infos);
 }
 
+pub fn get_netconn_list() -> Result<Vec<Netconn>, Box<dyn std::error::Error>> {
+    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
+
+    let hostname = rtevents::get_hostname();
+
+    let netconn_list: Vec<Netconn> = sockets_info
+        .into_iter()
+        .map(|si| {
+            let process_id_list = si.associated_pids.clone();
+
+            let conn_process_list: Vec<rtevents::Process> = process_id_list.into_iter().map(|pid|{
+                rtevents::get_process_by_id(pid)
+            }).collect();
+
+            let mut nc = Netconn {
+                hostname: hostname.clone(),
+                conntype: "*NA".to_string(),
+                local_address: "0.0.0.0".to_string(),
+                local_port: 0,
+                remote_address: "0.0.0.0".to_string(),
+                remote_port: 0,
+                state: "*NA".to_string(),
+                associated_pids: vec![0],
+                associated_processes: conn_process_list
+            };
+
+            match si.protocol_socket_info {
+                ProtocolSocketInfo::Udp(usi) => {
+                    nc.conntype = "UDP".to_string();
+                    nc.local_address = usi.local_addr.to_string();
+                    nc.local_port = usi.local_port; 
+                    nc.remote_address = usi.remote_addr.to_string(); 
+                    nc.remote_port = usi.remote_port;
+                    nc.associated_pids = si.associated_pids;
+                },  
+                ProtocolSocketInfo::Tcp(tsi) => {
+                    nc.conntype = "TCP".to_string();
+                    nc.local_address = tsi.local_addr.to_string();
+                    nc.local_port = tsi.local_port;
+                    nc.remote_address = tsi.remote_addr.to_string();
+                    nc.remote_port = tsi.remote_port;
+                    nc.state = tsi.state.to_string();
+                    nc.associated_pids = si.associated_pids;
+                }
+            };
+            nc
+        }).collect();
+
+        return Ok(netconn_list);
+}
