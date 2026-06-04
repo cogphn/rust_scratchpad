4e3675160b2cd4861606bcee8e028038e1c5d0d1
cogphn <jason.deyalsingh@gmail.com>
2026-06-04 04:48:05 -0400

    unclear

--- realive/src/main.rs
+++ realive/src/main.rs
@@ -11,17 +11,27 @@ use ollama_rs::{
     Ollama
 };
 
+use serde_json::json;
+
 use std::fs;
 use text_splitter::TextSplitter;
 use std::error::Error;
+use uuid::Uuid;
 
 const QDRANT_URL: &str = "http://dev05:6334";
-const BOOK_URL: &str = "C:\\users\\W530\\Downloads\\dmbok.md";
+//const BOOK_URL: &str = "C:\\users\\W530\\Downloads\\dmbok.md";
+const BOOK_URL: &str = "C:\\users\\W530\\Downloads\\rando_readme.md";
 const MODEL_NAME: &str = "nomic-embed-text";
 
 struct DocumentChunk {
     pub file_name: String,
     pub chunk: String
+}
+
+struct DocPoint {
+    pub point_id: String,
+    pub file_name: String,
+    pub vector: Vec<f32>
 }
 
 
@@ -63,12 +73,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("    [*] processing chunks...");
 
+    let mut points = Vec::new();
+
     for chunk in all_chunks {
         // get embedding I spose 
         let embedding_vector: Vec<f32> = get_embedding(chunk.chunk.as_str()).await?;
 
-        
-        
+        let point_id = Uuid::new_v4().to_string();
+
+        let point = DocPoint{
+            point_id: point_id,
+            file_name: BOOK_URL.to_string(),
+            vector: embedding_vector.clone()
+        };
+
+        points.push(point);
+
     }
 
 
